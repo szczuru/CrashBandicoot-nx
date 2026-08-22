46ae4da1d4422895c9fd9ed004e994bc1636b2c5
szczuru <szczuru@gmail.com>
2026-08-22 16:16:36 +0200

    Implement volume control in SwitchAudio
    Added volume control with clamping to the SwitchAudio class.

--- platform/switch/SwitchAudio.cs
+++ platform/switch/SwitchAudio.cs
@@ -4,6 +4,7 @@ public sealed class SwitchAudio
 {
     private int _sampleRate;
     private int _channels;
+    private float _volume = 1f;
 
     public void Init(int sampleRate = 44100, int channels = 2)
     {
@@ -17,11 +18,12 @@ public sealed class SwitchAudio
         _ = interleavedStereo;
         _ = _sampleRate;
         _ = _channels;
+        _ = _volume;
     }
 
     public void SetVolume(float volume01)
     {
-        _ = volume01;
+        _volume = Math.Clamp(volume01, 0f, 1f);
     }
 
     public void Shutdown()
