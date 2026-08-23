04c4c6d68ea1b5ceeb66aa79846e6241cea99beb
szczuru <szczuru@gmail.com>
2026-08-23 19:01:45 +0200

    Add XML documentation to ConfigManager class
    Added summary documentation for ConfigManager class.

--- RecompOne.Runtime/SlimStubs/ConfigManagerStub.cs
+++ RecompOne.Runtime/SlimStubs/ConfigManagerStub.cs
@@ -1,11 +1,10 @@
 namespace RecompOne.Runtime.Config;
 
+/// <summary>
+/// SWITCH_SLIM: zamiennik ConfigManager bez ImGui / paneli.
+/// GameConfig pochodzi z istniejącego pliku w Config/.
+/// </summary>
 public static class ConfigManager
 {
     public static GameConfig Game { get; } = new();
-}
-
-public sealed class GameConfig
-{
-    public bool CatalogDiscovery { get; set; }
 }
