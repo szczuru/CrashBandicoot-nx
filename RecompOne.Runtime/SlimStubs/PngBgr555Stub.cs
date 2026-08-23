43f38475ab5567f92ffbb48b6899bb7625078afa
szczuru <szczuru@gmail.com>
2026-08-23 19:07:41 +0200

    Add PngBgr555Stub class with TryEncodeFile method

---
+++ RecompOne.Runtime/SlimStubs/PngBgr555Stub.cs
@@ -0,0 +1,7 @@
+namespace RecompOne.Runtime.Catalogs;
+
+public static class PngBgr555
+{
+    public static bool TryEncodeFile(string path, ReadOnlySpan<ushort> pixels, int w, int h)
+        => false;
+}
