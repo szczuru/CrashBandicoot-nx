193909aa215c78ef9b993573185c17860ca1ddcd
szczuru <szczuru@gmail.com>
2026-08-23 19:06:59 +0200

    Add HostDesktopStubs with input and audio management

---
+++ RecompOne.Runtime/SlimStubs/HostDesktopStubs.cs
@@ -0,0 +1,28 @@
+namespace RecompOne.Runtime.Host;
+
+public static class InputManager
+{
+    public static void SetRumble(byte large, byte small) { }
+    public static void RequestFullscreenToggle() { }
+    public static void RequestCheatMenuToggle() { }
+    public static void RequestPauseMenuToggle() { }
+}
+
+public static class HostWindow
+{
+    public static void Initialize(string title) { }
+    public static void Present(object? gpu) { }
+    public static void Shutdown() { }
+    public static void WaitForValidDisc() { }
+    public static void SetEmbedParent(nint hwnd) { }
+    public static void FitEmbeddedToParent() { }
+    public static void SetFullscreen(bool on) { }
+}
+
+public static class Audio
+{
+    public static void Initialize() { }
+    public static void SetMasterVolume(float v) { }
+    public static void Attach(object? spu) { }
+    public static void Shutdown() { }
+}
