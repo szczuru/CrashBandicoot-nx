197063e1b94f50b5bc41490a9c3a8e38aaba9b58
szczuru <szczuru@gmail.com>
2026-08-23 19:13:13 +0200

    Update TextureReplacementsStub.cs

--- RecompOne.Runtime/SlimStubs/TextureReplacementsStub.cs
+++ RecompOne.Runtime/SlimStubs/TextureReplacementsStub.cs
@@ -2,11 +2,13 @@ namespace RecompOne.Runtime.Catalogs;
 
 public static class TextureReplacements
 {
+    public static bool HasAny => false;
+
+    public static bool TryApply(int x, int y, int w, int h, ushort[]? pixels, int count)
+        => false;
+
     public static bool TryRegisterPng(string id, ReadOnlySpan<byte> png) => false;
     public static bool TryRegisterPngFile(string id, string path) => false;
     public static void Remove(string id) { }
     public static void Clear() { }
-
-    public static bool TryApply(object? any) => false;
-    public static bool ShouldReplace(object? any) => false;
 }
