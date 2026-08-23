f0ba23e69edec6c28e6b5b7737bfc12eef2575ab
szczuru <szczuru@gmail.com>
2026-08-23 18:59:16 +0200

    Refactor TexturesCatalog class and add methods

--- RecompOne.Runtime/SlimStubs/TexturesCatalogStub.cs
+++ RecompOne.Runtime/SlimStubs/TexturesCatalogStub.cs
@@ -1,17 +1,42 @@
-namespace RecompOne.Runtime.Catalog;
+using System.Text.Json;
 
-/// <summary>
-/// Pusty katalog tekstur pod SWITCH_SLIM (bez StbImage / mod texture packs).
-/// </summary>
+namespace RecompOne.Runtime.Catalogs;
+
+/// <summary>SWITCH_SLIM: pusty katalog tekstur (bez PNG / ModLoader).</summary>
 public sealed class TexturesCatalog
 {
     public static TexturesCatalog Empty { get; } = new();
 
-    public void Clear() { }
+    public int Count => 0;
 
-    public bool TryGet(object? key, out object? value)
+    public static TexturesCatalog FromJson(JsonDocument? doc) => Empty;
+
+    internal bool TryResolve(
+        int x, int y, int w, int h, ushort[]? pixels, int count, out TextureInfo info)
     {
-        value = null;
+        info = null!;
         return false;
     }
+
+    public bool TryGet(string id, out TextureInfo info)
+    {
+        info = null!;
+        return false;
+    }
+
+    public void Replace(string id, ReadOnlySpan<byte> pngBytes) { }
+
+    public void Replace(string id, string pngPath) { }
+
+    public int ReplaceMany(IEnumerable<(string id, string pngPath)> entries) => 0;
+
+    public int ReplaceMany(IEnumerable<(string id, byte[] pngBytes)> entries) => 0;
+
+    public int ReplaceDirectory(string directory) => 0;
+
+    public void ClearReplace(string id) { }
+
+    public void ClearAllReplaces() { }
+
+    public bool ReloadModAssets() => false;
 }
